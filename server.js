9d5c94f7c85168b23784d0303c8ecfcee5ffd6d6
Sid <sidchopp@gmail.com>
2021-09-22 12:56:56 -0600

    transaction is used for register end point

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ app.post('/register', (req, res) => {
   var salt = bcrypt.genSaltSync(10);
   var hash = bcrypt.hashSync(password, salt);
 
+  // we created transaction when we do more than 2 things at ONCE, like when a new use rregister, we want to save its info in 'users' and 'login' table of our DB at ONCE and also for Correct recovery from failures
   db.transaction(trx => {
     trx.insert({
       // RHS hash is the const from above
@@ -73,13 +74,13 @@ app.post('/register', (req, res) => {
       // RHS email is from req.body above
       email: email
     })
-      .into('login')
+      .into('login') // 'login' is the name of table in DB
       .returning('email')
       .then(loginEmail => {
-        return trx('users')
+        return trx('users') //'users' is the name of table in DB
           .returning('*')
           .insert({
-            email: loginEmail,
+            email: loginEmail[0],
             name: name,
             joined: new Date()
           })
@@ -87,6 +88,8 @@ app.post('/register', (req, res) => {
             res.json(user[0])
           })
       })
+      .then(trx.commit)
+      .catch(trx.rollback)
   })
     // DATABASE.users.push({
     //   id: '3',
