29449c2012214caf64c31ec4218ab0c2404b4c1b
Sid <sidchopp@gmail.com>
2021-09-23 16:56:36 -0600

    error removed

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 var bcrypt = require('bcryptjs');
 var cors = require('cors')
 
-
 const db = require('knex')({
   client: 'pg',
   connection: {
@@ -19,27 +18,6 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-// const DATABASE = {
-//   users: [
-//     {
-//       id: '1',
-//       name: 'Sid',
-//       email: 'sid@gmail.com',
-//       password: 'hurray',
-//       entries: 0,
-//       joined: new Date()
-//     },
-//     {
-//       id: '2',
-//       name: 'Sally',
-//       email: 'sally@gmail.com',
-//       password: 'yahoo',
-//       entries: 0,
-//       joined: new Date()
-//     },
-//   ]
-// }
-
 app.get("/", (req, res) => {
   // res.send(DATABASE.users)
   res.send(db.users)
@@ -47,21 +25,10 @@ app.get("/", (req, res) => {
 })
 
 app.post("/signIn", (req, res) => {
-  // bcrypt.compare("trees", "$2a$10$Qyb3Pg2yqPWZs4Nm1nsZT.cZXP3AUA5R2jthUHVEgPSRu9SHOoaDi", function (err, res) {
-  //   console.log(res);
-
-  // });
-  // if (req.body.email === DATABASE.users[0].email && req.body.password === DATABASE.users[0].password) {
-  //   //res.json('Success!!')
-  //   res.json(DATABASE.users[0]);
-  // } else {
-  //   res.status(400).json('Error logging in..')
-  // }
   const { email, password } = req.body
   if (!email || !password) {
     return res.status(400).json('Please fill your email and password first.')
   }
-
   db.select('email', 'hash').from('login')
     .where('email', '=', req.body.email)
     .then(data => {
@@ -117,16 +84,6 @@ app.post('/register', (req, res) => {
       .then(trx.commit)
       .catch(trx.rollback)
   })
-    // DATABASE.users.push({
-    //   id: '3',
-    //   name: name,
-    //   email: email,
-    //   password: password,
-    //   entries: 0,
-    //   joined: new Date()
-    // })
-
-    //  .catch(err => res.status(400).json(err))
     .catch(err => res.status(400).json("Unable to register, as the Email address already exists!"))
 })
 
@@ -135,16 +92,6 @@ app.get('/profile/:id', (req, res) => {
   //req.params is the the "part of request" we send in the request URL parameter
 
   const { id } = req.params;
-
-  // DATABASE.users.forEach(user => {
-  //   //LHS id refers to what a user fills on UI and RHS id is from DATABASE
-  //   if (user.id === id) {
-  //     //If user input matches with its values in DATABASE,send that user
-  //     found = true;
-  //     return res.json(user)
-  //   }
-  // })
-
   db.select('*')
     .from('users')
     .where({
@@ -152,7 +99,6 @@ app.get('/profile/:id', (req, res) => {
     })
     .then(user => {
       if (user.length) {
-
         res.json(user[0])
       } else {
         res.status(400).json('Not Found!!')
@@ -165,19 +111,6 @@ app.get('/profile/:id', (req, res) => {
 // to update we use put rquest
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  // let found = false;
-  // DATABASE.users.forEach(user => {
-  //   if (user.id === ID) {
-  //     //If user input matches with its values in DATABASE,send that user
-  //     found = true;
-  //     user.entries++
-  //     return res.json(user.entries)
-  //   }
-  // })
-  // // and if it mismatches
-  // if (!found) {
-  //   res.status(400).json('not found')
-  // }
   db('users').where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
@@ -185,10 +118,9 @@ app.put('/image', (req, res) => {
       res.json(entries[0])
     })
     .catch(err => res.status(400).json('Unable to get entries'))
-
 })
 
 app.listen(3000, () => {
   console.log('app is running on 3000');
-})
+});
 
