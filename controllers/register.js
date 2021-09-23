ba955116e09604b300bac71ae3919f5ce7c60845
Sid <sidchopp@gmail.com>
2021-09-23 17:38:51 -0600

    seperate controller for register is created

---
+++ controllers/register.js
@@ -0,0 +1,41 @@
+const handleRegister = (req, res, db, bcrypt) => {
+  const { email, password, name } = req.body
+
+  if (!email || !name || !password) {
+    return res.status(400).json('In order to register,please fill all the three fields!')
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
+  // we created transaction when we do more than 2 things at ONCE, like when a new use rregister, we want to save its info in 'users' and 'login' table of our DB at ONCE and also for Correct recovery from failures
+  db.transaction(trx => {
+    trx.insert({
+      // RHS hash is the const from above
+      hash: hash,
+      // RHS email is from req.body above
+      email: email
+    })
+      .into('login') // 'login' is the name of table in DB
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users') //'users' is the name of table in DB
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0])
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+  })
+    .catch(err => res.status(400).json("Unable to register, as the Email address already exists!"))
+}
+
+module.exports = {
+  handleRegister: handleRegister
+}
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 var bcrypt = require('bcryptjs');
-var cors = require('cors')
+var cors = require('cors');
+const register = require('./controllers/register');
+
+//components
 
 const db = require('knex')({
   client: 'pg',
@@ -48,44 +51,7 @@ app.post("/signIn", (req, res) => {
     .catch(err => res.status(400).json('Wrong Credentials!😞 '))
 })
 
-app.post('/register', (req, res) => {
-  const { email, password, name } = req.body
-
-  if (!email || !name || !password) {
-    return res.status(400).json('In order to register,please fill all the three fields!')
-  }
-
-  const bcrypt = require('bcryptjs');
-  const salt = bcrypt.genSaltSync(10);
-  const hash = bcrypt.hashSync(password, salt);
-
-  // we created transaction when we do more than 2 things at ONCE, like when a new use rregister, we want to save its info in 'users' and 'login' table of our DB at ONCE and also for Correct recovery from failures
-  db.transaction(trx => {
-    trx.insert({
-      // RHS hash is the const from above
-      hash: hash,
-      // RHS email is from req.body above
-      email: email
-    })
-      .into('login') // 'login' is the name of table in DB
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users') //'users' is the name of table in DB
-          .returning('*')
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0])
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-  })
-    .catch(err => res.status(400).json("Unable to register, as the Email address already exists!"))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
 
